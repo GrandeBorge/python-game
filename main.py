88c32096393fa5b9410a7b41082ebcfae9ffe44e
studente <studente@liceospalla.local>
2019-02-27 09:37:43 +0100

    Creazione della classe World

--- main.py
+++ main.py
@@ -7,11 +7,30 @@ class Entity:
 
 e = Entity(5, 5, "X")
 
-for y in range(10):
-  for x in range(10):
-    if e.x == x and e.y == y:
-      print("[{}]".format(e.graphic), end="")
-    else:
-      print("[ ]", end="")
+class World:
 
-  print()
+  def __init__(self, map_x, map_y):
+
+    self.map_x = map_x
+    self.map_y = map_y
+
+  def draw(self, *n):
+
+    a = 0
+    for y in range(self.map_y):
+
+      for x in range(self.map_x):
+
+        for e in n:
+
+          if e.y-1 == y:
+            if e.x-1 == x:
+              print("[{}]".format(e.graphic), end = "")
+              a = 1
+        if a == 0:
+          print("[ ]", end="")
+        else: 
+          a = 0
+          
+      print()
+
