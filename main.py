ea724d6d05ceb178f932dd7e5e498897aa10f733
studente <studente@liceospalla.local>
2019-02-27 09:26:33 +0100

    Creata la classe World(Prima versione)

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ class Entity:
 
 e = Entity(5, 5, "X")
 
-for y in range(10):
-  for x in range(10):
-    if e.x == x and e.y == y:
-      print("[{}]".format(e.graphic), end="")
-    else:
-      print("[ ]", end="")
+class World:
+  def draw(lenght,height):
+    for x in range(lenght):
+      for y in range(height):
+        if e.x == x and e.y == y:
+          print("[{}]".format(e.graphic), end="")
+        else:    
+          print("[ ]", end="")
+      print()
 
-  print()
+l = int(input("Definisci la lunghezza del campo > "))
+h = int(input("Definisci l'altezza del campo > "))
+
+World.draw(l,h)
+       
+     
+
