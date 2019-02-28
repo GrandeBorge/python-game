7a6d644e44ef3db9dd4d842ef4e0a6b94bddccb9
Kevin Muka <kevinmuka@Air-di-Kevin.mshome.net>
2019-02-28 09:31:22 +0100

    Sistemato World

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 class Entity:
 
-	def __init__(self, x, y, graphic):
-		self.x = x
-		self.y = y
-		self.graphic = graphic
+  def __init__(self, x, y, graphic):
+    self.x = x
+    self.y = y
+    self.graphic = graphic
 
 e = Entity(5, 9, "X")
 
 class World:
 
-	def __init__(self, map_x, map_y):
-		self.map_x = map_x
-		self.map_y = map_y
+  def __init__(self, map_x, map_y):
+    self.map_x = map_x
+    self.map_y = map_y
 
-	def draw(self, *n):
-		a = True
+  def draw(self, *n):
+    for y in range(self.map_y):
+      for x in range(self.map_x):
+        for e in n:
+          if (e.x - 1 == x) and (e.y - 1) == y:
+            print("[{}]".format(e.graphic), end = "")
+            break
+        else:
+          print("[ ]", end = "")
 
-		for y in range(self.map_y):
-			for x in range(self.map_x):
-
-				for e in n:
-					if (e.x - 1, e.y - 1) == (x, y):
-						print("[{}]".format(e.graphic), end = "")
-						a = False
-
-				if a == True:
-					print("[ ]", end="")
-				else: 
-					a = True
-
-			print()
+      print()
