3e35a3a49761630cc184602823945711267dd02b
Gianluca Ghinazzi <gianluca.ghinazzi@gmail.com>
2019-02-28 09:42:02 +0100

    aggiunta classe move

--- main.py
+++ main.py
@@ -16,7 +16,23 @@ for y in range(10):
 
   print()
 
-class Player:
+class Move:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+  def move(self, direction):
+    while direction != "A" or "S" or "D" or "W" or "a" or "s" or "w" or "d":
+      direction = input("Inserisci W, A, S o D")
+    if direction == "A" or "a":
+      self.y -= 1
+    elif direction == "d" or "D":
+      self.y += 1
+    elif direction == "w" or "W":
+      self.x -= 1
+    elif direction == "s" or "S":
+      self.x += 1
+
+  class Player:
   def __init__(self, x, y, health, damage):
     self.x = x
     self.y = y
