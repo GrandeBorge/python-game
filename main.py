eae9eb55f0e6efc5c54362168db959aff98e3c2f
Kevin Muka <kevinmuka@Air-di-Kevin.mshome.net>
2019-02-28 09:42:57 +0100

    Sistemati i nomi delle variabili della classe World

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ e = Entity(5, 9, "X")
 
 class World:
 
-  def __init__(self, map_x, map_y):
-    self.map_x = map_x
-    self.map_y = map_y
+  def __init__(self, w, h):
+    self.w = w
+    self.h = h
 
   def draw(self, *n):
-    for y in range(self.map_y):
-      for x in range(self.map_x):
+    for y in range(self.h):
+      for x in range(self.w):
         for e in n:
           if (e.x - 1) == x and (e.y - 1) == y:
             print("[{}]".format(e.graphic), end = "")
